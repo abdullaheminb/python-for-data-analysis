96b385bc87cb0dab77dd1973025cbf76d8ca65ec
Abdullah Bayraklilar <abdullahbayraklilar@gmail.com>
2023-11-26 05:35:34 +0300

    Updated chapter 3

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,14 @@
-print('hello world')
+all_data = [
+    ['John', 'Emily', 'Michael', 'Mary', 'Steven'], 
+    ['Maria', 'Juan', 'Javier', 'Natalia', 'Pilar']
+    ]
+
+#find all names with more than one a
+
+names_of_interest = []
+
+for names in all_data:
+    enough_as = [name for name in names if name.count('a')>=2]
+    names_of_interest.extend(enough_as)
+
+print(names_of_interest)
